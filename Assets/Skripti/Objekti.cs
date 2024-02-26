40d63f7073eb7e53ea1912e8d0730e93803f4db6
Lauris P <porinslauris@gmail.com>
2024-02-26 19:42:08 +0200

    Pielikti objekti, Kameru testesana

---
+++ Assets/Skripti/Objekti.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objekti : MonoBehaviour
+{
+    public GameObject izvelesLauks;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Skripti/SpelesKontrole.cs
+++ Assets/Skripti/SpelesKontrole.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(PolygonCollider2D))]
 
@@ -10,6 +11,7 @@ public class SpelesKontrole : MonoBehaviour
 
     public Valstis valsts;
     public ValstsParvalde parvalde;
+    public Objekti objekti;
     private SpriteRenderer sprite;
 
     public List<GameObject> valstsSaraksts = new List<GameObject>();
@@ -18,7 +20,11 @@ public class SpelesKontrole : MonoBehaviour
     public Color32 hoverKrasa;
     //Mainīgie
     
-    
+
+    void Start(){
+        objekti = FindObjectOfType<Objekti>();
+    }
+
     void Awake()
     {
         sprite = GetComponent<SpriteRenderer>();
@@ -27,14 +33,22 @@ public class SpelesKontrole : MonoBehaviour
     void OnMouseEnter()
     {
         vecaKrasa = sprite.color;
-         if (valsts.speletajs == Valstis.Speletaji.LSPR){
-              hoverKrasa = new Color32(vecaKrasa.r, vecaKrasa.g, vecaKrasa.b, 255);
+         if (valsts.speletajs == Valstis.Speletaji.PLAYER){
+              hoverKrasa = new Color32(vecaKrasa.r, vecaKrasa.g, vecaKrasa.b, 200);
          }
         if (valsts.speletajs == Valstis.Speletaji.LSPR)
         {
-            hoverKrasa = new Color32(vecaKrasa.r, vecaKrasa.g, vecaKrasa.b, 200);
+            hoverKrasa = new Color32(vecaKrasa.r, vecaKrasa.g, vecaKrasa.b, 255);
         }
         sprite.color = hoverKrasa;
+    }
+
+    public void OnMouseClick(){
+         if (valsts.speletajs == Valstis.Speletaji.PLAYER && Input.GetKeyDown(KeyCode.Mouse0)){
+             objekti.izvelesLauks.gameObject.SetActive(true);
+             Debug.Log("Nokliķšķinās!");
+         }
+         Debug.Log("Nokliķšķinās!");
     }
 
      void OnMouseExit()
